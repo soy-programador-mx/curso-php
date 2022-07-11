414ece93bf1539ac63ff6b38cf81e4222d36e746
Soy programador <soytecnologicomx@gmail.com>
2022-07-10 19:20:42 -0500

    Principios básicos

---
+++ principios_basicos/etiquetas_php.php
@@ -0,0 +1,10 @@
+<?php
+// Imprime el texto de "Hola Mundo"
+echo 'Hola Mundo'.' <br />';
+
+/* Imprime el texto "Hola amigo"  
+asdsadasdasdas
+asdasdasdasdasd
+asdasdasdasd
+*/
+echo "Hola amigo";
---
+++ principios_basicos/html_con_php.php
@@ -0,0 +1,3 @@
+<p>Esto es un parrafo <?php echo ' en HTML'; ?></p>
+
+<?php echo 'Hola de nuevo'; ?>
